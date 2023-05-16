c6a07e6833ee6071e1eac20560be172401609e9a
Bruna Cabral <brunasc7@gmail.com>
2023-05-16 12:36:06 -0300

    Mudança no JS e no CSS

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,15 @@
-const botaoAterarTema = document.getElementById("botao-alterar-tema")
-const body = document.querySelector("body")
-const imagemBotaoAlterarTema = document.querySelector(".imagem-botao")
-const modoEscutoAtivo = body.classList.contains("modo-escuro")
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
 
-botaoAterarTema.addEventListener("click", () => {
-    imagemBotaoAlterarTema.setAttribute("src", "../src/imagens/moon.png")
-    body.classList.toggle("modo-escuro");
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
 
-    if (modoEscutoAtivo) {
-        imagemBotaoAlterarTema.setAttribute("src", "../src/imagens/sun.png")
-    } else {
-        imagemBotaoAlterarTema.setAttribute("src", "../src/imagens/moon.png")
-    }
-})
+  body.classList.toggle("modo-escuro");
 
-
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+  }
+});
